17b395560b042c5b163cd28bb10bfd4d60f1f9d8
Surma <surma@surma.link>
2020-02-28 19:06:12 +0000

    Move letterbox to its own module

--- src/worker.js
+++ src/worker.js
@@ -13,49 +13,7 @@
 
 import { expose } from "comlink";
 
-import { modulePromise } from "asc:./letterbox.as";
-
-async function letterbox(image, aspectW, aspectH, r, g, b, a) {
-  const sourceRatio = image.width / image.height;
-  const targetRatio = aspectW / aspectH;
-  let targetWidth, targetHeight;
-  if (targetRatio >= sourceRatio) {
-    targetHeight = image.height;
-    targetWidth = image.height * targetRatio;
-  } else {
-    targetWidth = image.width;
-    targetHeight = image.width / targetRatio;
-  }
-  const targetImageSize = targetWidth * targetHeight * 4;
-  const bufferSize = image.data.byteLength + targetImageSize;
-  const numPages = Math.ceil(bufferSize / (64 * 1024));
-  const memory = new WebAssembly.Memory({ initial: numPages });
-  const instance = await WebAssembly.instantiate(await modulePromise, {
-    env: { memory }
-  });
-  new Uint8ClampedArray(memory.buffer, 0, image.data.byteLength).set(
-    image.data
-  );
-  instance.exports.letterbox(
-    image.width,
-    image.height,
-    targetWidth,
-    targetHeight,
-    r,
-    g,
-    b,
-    a
-  );
-
-  const data = new Uint8ClampedArray(
-    memory.buffer,
-    image.data.byteLength,
-    targetImageSize
-  );
-  // Need to slice the data here to work around a bug in Chrome.
-  // https://crbug.com/1056661
-  return new ImageData(data.slice(), targetWidth, targetHeight);
-}
+import { letterbox } from "./letterbox.js";
 
 expose({
   letterbox
