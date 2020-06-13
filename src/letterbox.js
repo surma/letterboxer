26281412769a200e100a2bd50dda393fa0c74b34
Surma <surma@surma.dev>
2020-06-13 13:40:15 +0100

    Switch to OffscreenCanvas

--- src/letterbox.js
+++ src/letterbox.js
@@ -1,5 +1,3 @@
-import { modulePromise } from "asc:./letterbox.as";
-
 export async function letterbox(image, aspectW, aspectH, r, g, b, a) {
   const sourceRatio = image.width / image.height;
   const targetRatio = aspectW / aspectH;
@@ -11,29 +9,13 @@ export async function letterbox(image, aspectW, aspectH, r, g, b, a) {
     targetWidth = image.width;
     targetHeight = Math.round(image.width / targetRatio);
   }
-  const targetImageSize = targetWidth * targetHeight * 4;
-  const bufferSize = targetImageSize;
-  const numPages = Math.ceil(bufferSize / (64 * 1024));
-  const memory = new WebAssembly.Memory({ initial: numPages });
-  const instance = await WebAssembly.instantiate(await modulePromise, {
-    env: { memory }
-  });
-  new Uint8ClampedArray(memory.buffer, 0, image.data.byteLength).set(
-    image.data
-  );
-  instance.exports.letterbox(
-    image.width,
-    image.height,
-    targetWidth,
-    targetHeight,
-    r,
-    g,
-    b,
-    a
-  );
+  const imageStartX = (targetWidth - image.width) / 2;
+  const imageStartY = (targetHeight - image.height) / 2;
+  const canvas = new OffscreenCanvas(targetWidth, targetHeight);
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = `rgba(${r},${g},${b},${a})`;
+  ctx.fillRect(0, 0, targetWidth, targetHeight);
+  ctx.putImageData(image, imageStartX, imageStartY);
 
-  const data = new Uint8ClampedArray(memory.buffer, 0, targetImageSize);
-  // Need to slice the data here to work around a bug in Chrome.
-  // https://crbug.com/1056661
-  return new ImageData(data.slice(), targetWidth, targetHeight);
+  return ctx.getImageData(0, 0, targetWidth, targetHeight);
 }
