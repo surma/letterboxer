753b8ced214b0ac2dce219f442f2f3ae211b0176
Surma <surma@surma.dev>
2020-07-09 16:23:30 +0100

    Don't scale image when using borders

--- src/letterbox.js
+++ src/letterbox.js
@@ -11,19 +11,15 @@ export async function letterbox(image, aspectW, aspectH, border, r, g, b, a) {
     targetWidth = image.width;
     targetHeight = Math.round(image.width / targetRatio);
   }
-  const imageStartX = (targetWidth - image.width) / 2 + border;
-  const imageStartY = (targetHeight - image.height) / 2 + border;
+  targetWidth += 2 * border;
+  targetHeight += 2 * border;
+  const imageStartX = (targetWidth - image.width) / 2;
+  const imageStartY = (targetHeight - image.height) / 2;
   const canvas = new OffscreenCanvas(targetWidth, targetHeight);
   const ctx = canvas.getContext("2d");
   ctx.fillStyle = `rgba(${r},${g},${b},${a})`;
   ctx.fillRect(0, 0, targetWidth, targetHeight);
-  ctx.drawImage(
-    image,
-    imageStartX,
-    imageStartY,
-    image.width - 2 * border,
-    image.height - 2 * border
-  );
+  ctx.drawImage(image, imageStartX, imageStartY, image.width, image.height);
 
   return ctx.getImageData(0, 0, targetWidth, targetHeight);
 }
