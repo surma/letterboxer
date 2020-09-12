487165235d8aa14da39cde6827a9d0b776a1c211
Surma <surma@surma.dev>
2020-09-12 21:36:16 +0100

    Fix downloading

--- src/main.js
+++ src/main.js
@@ -126,9 +126,10 @@ async function main() {
     )
     .pipeThrough(
       forEach(async imageData => {
-        const { view, canvas } = await resultViewPromise;
-        imageDataToCanvas(imageData, canvas);
+        const { view, setCanvas } = await resultViewPromise;
+        const canvas = imageDataToCanvas(imageData);
         render(output, view);
+        setCanvas(canvas);
       })
     )
     .pipeTo(discard());
--- src/views/result.js
+++ src/views/result.js
@@ -3,35 +3,43 @@ import { downloadBlob } from "../dom-utils.js";
 import { canvasToBlob } from "../image-utils.js";
 
 let canvas;
+let jpegLink;
+let pngLink;
 let back;
+
+async function setCanvas(cvs) {
+  canvas.replaceWith(cvs);
+  canvas = cvs;
+  const jpegBlob = await canvasToBlob(canvas, "image/jpeg", 100);
+  const jpegFile = new File([jpegBlob], "image.jpeg", { type: "image/jpeg" });
+  jpegLink.href = URL.createObjectURL(jpegFile);
+
+  const pngBlob = await canvasToBlob(canvas, "image/png");
+  const pngFile = new File([pngBlob], "image.png", { type: "image/png" });
+  pngLink.href = URL.createObjectURL(pngFile);
+}
+
 const view = (
   <>
     <style>{`
       canvas {
         max-width: 100%;
       }
+	  #downloadbtns a {
+	    border: 1px solid black;
+	    margin: 1em;
+	    padding: 1em;
+	    display: inline-block;
+	  }
     `}</style>
     {(back = <button>Back</button>)}
     {(canvas = <canvas />)}
-    <button
-      onclick={async () => {
-        const blob = await canvasToBlob(canvas, "image/jpeg", 100);
-        const file = new File([blob], "image.jpeg", { type: "image/jpeg" });
-        downloadBlob(file);
-      }}
-    >
-      JPEG
-    </button>
-    <button
-      onclick={async () => {
-        const blob = await canvasToBlob(canvas, "image/png");
-        const file = new File([blob], "image.png", { type: "image/png" });
-        downloadBlob(file);
-      }}
-    >
-      PNG
-    </button>
+    <fieldset id="downloadbtns">
+      <legend>Download:</legend>
+      {(jpegLink = <a download="image.jpg">JPEG</a>)}
+      {(pngLink = <a download="image.png">PNG</a>)}
+    </fieldset>
   </>
 );
 
-export { view, canvas, back };
+export { view, setCanvas, back };
